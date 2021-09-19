40b5eb67f0873d142601b1cf4667f492c5283f2f
Travis Spomer <16262858+TravisSpomer@users.noreply.github.com>
2021-09-18 17:43:40 -0700

    Nicer unread threads pager

--- src/lib/components/UnreadThreadPager.svelte
+++ src/lib/components/UnreadThreadPager.svelte
@@ -6,39 +6,139 @@
 </script>
 
 <style lang="scss">
-	
-	.root
+	@import "../../core.scss";
+
+	a
 	{
 		display: flex;
 		justify-content: flex-end;
-		margin-top: 3em;
+		margin: 2em (-$indent * 0.5) 1em (-$indent * 0.5);
+		padding: 1em ($indent * 0.5);
 
-		/* TODO: Replace this with an "outline" button style */
-		a
+		text-decoration: none;
+
+		transition: background-color 125ms ease;
+
+		&.back
 		{
-			padding: 0.5em 1em;
+			justify-content: flex-start;
 
-			color: var(--primary-dark4);
-
-			background-color: var(--primary-light4);
-			border: 1px solid var(--primary-dark2);
-			border-radius: 8px;
-
-			transition: background-color 133ms;
-
-			&:hover
+			.arrow
 			{
-				background-color: var(--background);
+				margin: 0 ($indent * 0.5);
+			}
+		}
+
+		.arrow
+		{
+			margin: 0 ($indent * 0.5);
+
+			color: var(--grey);
+			font-size: 48px;
+
+			user-select: none;
+
+			transition: translate 125ms ease;
+		}
+
+		.vertical
+		{
+			display: flex;
+			flex-direction: column;
+		}
+
+		.heading
+		{
+			color: var(--grey-dark1);
+		}
+
+		.thread-title
+		{
+			font-size: 28px;
+			line-height: 40px;
+			font-weight: $font-weight-bold;
+
+			color: var(--primary-dark2);
+		}
+
+		&:hover, &:focus
+		{
+			background-color: var(--grey-light4);
+			outline: none;
+
+			.arrow
+			{
+				translate: 0.125em 0;
+
+				color: var(--primary-dark2);
+			}
+
+			.heading
+			{
+				color: var(--foreground);
+			}
+
+			.thread-title
+			{
+				text-decoration: underline;
+			}
+
+			&.back
+			{
+				.arrow
+				{
+					translate: -0.125em 0;
+				}
+			}
+		}
+
+		&:focus:not(:focus-visible):not(:hover)
+		{
+			background-color: unset;
+
+			.arrow
+			{
+				translate: 0;
+
+				color: var(--grey);
+			}
+
+			.heading
+			{
+				color: var(--grey-dark1);
+			}
+
+			.thread-title
+			{
+				text-decoration: none;
+			}
+
+			&.back
+			{
+				.arrow
+				{
+					translate: 0;
+				}
 			}
 		}
 	}
 
 </style>
 
-<nav class="root">
-	{#if $unreadThreads.next}
-		<a href="/threads/{$unreadThreads.next.id}" sveltekit:noscroll title="{$unreadThreads.length} unread">Next: {$unreadThreads.next.title}&nbsp;<strong>›</strong></a>
-	{:else if thenHome}
-		<a href="/" title="No unread threads"><strong>‹</strong>&nbsp;Back home</a>
-	{/if}
-</nav>
+{#if $unreadThreads.next}
+	<a class="forward" href="/threads/{$unreadThreads.next.id}" sveltekit:noscroll title="{$unreadThreads.length} unread">
+		<div class="vertical">
+			<div class="heading">Next up</div>
+			<div class="thread-title">{$unreadThreads.next.title}</div>
+		</div>
+		<div class="arrow">›</div>
+	</a>
+{:else if thenHome}
+	<a class="back" href="/" title="No unread threads">
+		<div class="arrow">‹</div>
+		<div class="vertical">
+			<div class="heading">That’s it for now</div>
+			<div class="thread-title">Back home</div>
+		</div>
+	</a>
+{/if}
