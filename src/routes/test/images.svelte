98ccdff59610c7a5d6220f8b2431b2658d243a51
Travis Spomer <16262858+TravisSpomer@users.noreply.github.com>
2022-04-24 14:11:03 -0700

    More test cases

--- src/routes/test/images.svelte
+++ src/routes/test/images.svelte
@@ -8,6 +8,12 @@
 		`<p>Large landscape with explicit width and height</p><p><img src="https://images.unsplash.com/photo-1648737119359-510d4f551382" width="6048" height="4024" /></p>`,
 		`<p>Large portrait without width and height</p><p><img src="https://images.unsplash.com/photo-1621569898825-ef12e7592f94" /></p>`,
 		`<p>Large portrait with explicit width and height</p><p><img src="https://images.unsplash.com/photo-1621569898825-ef12e7592f94" width="3456" height="4608" /></p>`,
+		`<p>Small square without width and height</p><p><img src="/images/app/logo192.png" /></p>`,
+		`<p>Small square with explicit width and height</p><p><img src="/images/app/logo192.png" width="192" height="192" /></p>`,
+		`<p>Medium landscape with just a width</p><p><img src="https://images.unsplash.com/photo-1648737119359-510d4f551382" width="400" /></p>`,
+		`<p>Medium landscape with just a height</p><p><img src="https://images.unsplash.com/photo-1648737119359-510d4f551382" height="300" /></p>`,
+		`<p>Medium portrait with just a width</p><p><img src="https://images.unsplash.com/photo-1621569898825-ef12e7592f94" width="300" /></p>`,
+		`<p>Medium portrait with just a height</p><p><img src="https://images.unsplash.com/photo-1621569898825-ef12e7592f94" height="400" /></p>`,
 	]
 </script>
 
