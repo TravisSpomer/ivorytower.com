008d514dc642b6fb121dd6f44173765a4821f65c
Travis Spomer <16262858+TravisSpomer@users.noreply.github.com>
2022-04-28 11:12:39 -0700

    Use @const

--- src/lib/components/ThreadView.svelte
+++ src/lib/components/ThreadView.svelte
@@ -46,10 +46,11 @@
 <section>
 	{#if !$preferences.threadsShowNewestPostsFirst}
 		{#each thread.posts as post, i (`${thread.id}/${post.id}`)}
-			{#if i > 0 && i === thread.posts.length - thread.unread }
+			{@const firstUnreadPost = thread.posts.length - thread.unread}
+			{#if i > 0 && i === firstUnreadPost}
 				<Divider label={thread.unread > 1 ? `${thread.unread} new posts` : "new post"} highlight />
 			{/if}
-			<PostView {post} scrollIntoView={scrollIntoView && i === thread.posts.length - thread.unread} unread={i >= thread.posts.length - thread.unread} on:reply />
+			<PostView {post} scrollIntoView={scrollIntoView && i === firstUnreadPost} unread={i >= firstUnreadPost} on:reply />
 			{#if i === 0 && clipped > 0}
 				<Divider interactive={!loading} waiting={loading} label="{loading ? "Getting " : "See "}{clipped} older posts" on:click={onShowAll} />
 			{/if}
