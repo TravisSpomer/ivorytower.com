9c639c87f05e31d3d5cf01bfb8df9984ce27908e
Travis Spomer <16262858+TravisSpomer@users.noreply.github.com>
2026-02-18 22:45:53 -0800

    Migrate AnimateValue

--- src/lib/components/AnimateValue.svelte
+++ src/lib/components/AnimateValue.svelte
@@ -1,36 +1,48 @@
 <script lang="ts">
+	import { backOut } from "svelte/easing"
+	import { fly } from "svelte/transition"
+	import { animations } from "$lib/utils/settings"
+	import { sizeToCss } from "$lib/utils/size"
+	import { untrack } from "svelte"
 
-import { backOut } from "svelte/easing"
-import { fly } from "svelte/transition"
-import { animations } from "$lib/utils/settings"
-import { sizeToCss } from "$lib/utils/size"
+	export interface Props
+	{
+		/** The current value to display. */
+		value: string | number
+		/** The number of milliseconds to animate between each item and the next one. */
+		animateDuration?: number
+		/** The height of each item, in pixels. Typically this should be equal fo the line height. */
+		itemHeight: number
+		/** The alignment of each item, as a valid align-items value. */
+		alignItems?: "center" | "flex-start" | "flex-end"
+		/** The minimum width of the container. */
+		minWidth?: number | string | undefined
+	}
 
-/** The current value to display. */
-export let value: string | number
-/** The number of milliseconds to animate between each item and the next one. */
-export let animateDuration: number = 300
-/** The height of each item, in pixels. Typically this should be equal fo the line height. */
-export let itemHeight: number
-/** The alignment of each item, as a valid align-items value. */
-export let alignItems: "center" | "flex-start" | "flex-end" = "center"
-/** The minimum width of the container. */
-export let minWidth: number | string | undefined = undefined
+	const {
+		value,
+		animateDuration = 300,
+		itemHeight,
+		alignItems = "center",
+		minWidth,
+	}: Props = $props()
 
-let valueArray: (string | number)[] = []
-let translationMultiplier: number
-const numberFormat = new Intl.NumberFormat().format
+	let oldValue: string | number = ""
+	const valueArray: (string | number)[] = $state([])
+	let translationMultiplier: number = $state(0)
+	const numberFormat = new Intl.NumberFormat().format
 
-// TODO: Migrate this to Svelte 5—not sure how!
+	$effect(() =>
+	{
+		translationMultiplier = typeof value !== "number" || typeof oldValue !== "number" || value >= oldValue ? 1 : -1
+		oldValue = value
 
-$:
-{
-	const oldValue = valueArray.length ? valueArray[0] : null
-	translationMultiplier = typeof value !== "number" || typeof oldValue !== "number" || value >= oldValue ? 1 : -1
-
-	valueArray.pop()
-	valueArray.push(value)
-	valueArray = valueArray
-}
+		untrack(() =>
+		{
+			valueArray.pop()
+			valueArray.push(value)
+		})
+	})
 
 </script>
 
