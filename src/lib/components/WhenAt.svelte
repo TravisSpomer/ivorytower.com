67a709ff68aa4aa6cce118cff547369a0a6fc1de
Travis Spomer <16262858+TravisSpomer@users.noreply.github.com>
2026-02-19 08:55:20 -0800

    Migrate WhenAt

--- src/lib/components/WhenAt.svelte
+++ src/lib/components/WhenAt.svelte
@@ -1,24 +1,53 @@
+<!--
+	Lets you quickly make styling choices in a layout based on the user's current location. For example, you could make nav bar links bold when the user
+	is already in that section of the site.
+
+	Usage:
+	<WhenAt href="/help" allowChildren>
+		{#snippet children({ current })}
+			<div style:font-weight={current ? "bold" : "unset"}>Help</div>
+		{/snippet}
+	</WhenAt>
+
+	Note that the content is rendered regardless of the match! (allowChildren tests for /help and /help/page1; if false, it would only test for /help.)
+-->
+
 <script lang="ts">
-	// TODO: Manually upgrade to Svelte 5
-
+	import type { Snippet } from "svelte"
+	import type { AfterNavigate } from "@sveltejs/kit"
 	import { onMount } from "svelte"
+	import { afterNavigate } from "$app/navigation"
 
-	/** When the current page location matches this href, the "current" prop on the outer component is set to true. */
-	export let href: string
-	/** If true, the "current" prop will also be true if the link points to a parent of the current page. */
-	export let allowChildren: boolean = false
-	/** If true, the query strings (searches) of the URLs will be ignored. */
-	export let ignoreQuery: boolean = true
-	/** If true, the hashes (anchors) of the URLs will be ignored. */
-	export let ignoreHash: boolean = true
+	interface Props {
+		/** When the current page location matches this href, the "current" prop on the snippet is set to true. */
+		href: string
+		/** If true, the "current" prop will also be true if the link points to a parent of the current page. */
+		allowChildren?: boolean
+		/** If true, the query strings (searches) of the URLs will be ignored. */
+		ignoreQuery?: boolean
+		/** If true, the hashes (anchors) of the URLs will be ignored. */
+		ignoreHash?: boolean
+		/** The content to render, regardless of whether the current page location is a match. */
+		children: Snippet<[{current: boolean}]>
+	}
 
-	let current: boolean = false
+	const {
+		href,
+		allowChildren = false,
+		ignoreQuery = true,
+		ignoreHash = true,
+		children,
+	}: Props = $props()
 
-	function checkMatch(): void
+	let current: boolean = $state(false)
+
+	function checkMatch(_navigation?: AfterNavigate): void
 	{
 		current = urlsMatch(location.href, href, allowChildren, ignoreQuery, ignoreHash)
+		console.log("checkMatch", current)
 	}
 	onMount(checkMatch)
+	afterNavigate(checkMatch)
 
 	/**
 		Tests to see if two URLs are approximately equal.
@@ -46,6 +75,4 @@
 	}
 </script>
 
-<svelte:window on:sveltekit:navigation-end={checkMatch} />
-
-<slot current={current} />
+{@render children({ current })}
